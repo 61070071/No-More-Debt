dc2761fba76fc47bddb50dd9a80a6399af41606e
thanapon chanlaor <42956113+61070071@users.noreply.github.com>
2019-04-15 22:04:48 +0700

    Update No More Debt.c

--- Project/No More Debt.c
+++ Project/No More Debt.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char student[10],add[10]; // cm,up,exเปนแค่ตัวเช็คค่าstringอย่าไปใส่ใจ
-    int check,cm,up,ex,money; // money คือ เงินที่จะบวกหรือลบให้กับ id นั้นๆ
+    char student[10],add[10]; // cm,up,ex,mo เปนแค่ตัวเช็คค่า string อย่าไปใส่ใจ
+    int check,cm,up,ex,mo,money,person = 10; // money คือ เงินที่จะบวกหรือลบให้กับ id นั้นๆ
     struct data
     {
         int id,debt;
-    }user[10];
-    for(int i = 0; i < 10; i++){
+    }user[person];
+    for(int i = 0; i < person; i++){
         user[i].id = 61070001 + i;
         user[i].debt = 0;
     }
@@ -18,6 +18,7 @@ int main(){
             scanf("%s",add);
             up = strcmp(add,"update");
             ex = strcmp(add,"exit");
+            mo = strcmp(add,"month");
             if (up == 0) {
         scanf("%d",&check);
         check -= 61070001;
@@ -25,18 +26,20 @@ int main(){
         scanf("%d",&money);
         user[check].debt += money;
             }
+            else if (mo == 0) {
+        scanf("%d",&money);
+        for (int i = 0; i < person; i++){
+            user[i].debt += money;
+        }  
+            }
         if (ex == 0) {
             break;
             }
         }
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < person; i++){
         printf("%d\n",user[i].id);
         printf("%d\n",user[i].debt);
         }
-        
-
     }
-        
-
     return 0;
 }
